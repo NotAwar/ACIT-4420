42aa37caec8471937260c034f5cecded9d3f9a7f
NotAwar <awar0201@gmail.com>
2024-09-11 01:08:18 +0200

    added savings account with interest

--- Mandatory_Assignment_1/SectionB.py
+++ Mandatory_Assignment_1/SectionB.py
@@ -15,4 +15,14 @@ class BankAccount:
     
     def account_info(self):
         return f"Account holder: {self.account_holder}, Balance: ${self.balance:.2f}"
+
+
+class SavingsAccount(BankAccount):
+    def __init__(self, account_holder, interest_rate=0.02):
+        super().__init__(account_holder)  # Initialize BankAccount attributes
+        self.interest_rate = interest_rate
     
+    def apply_interest(self):
+        interest = self.balance * self.interest_rate
+        self.balance += interest
+        print(f"Applied interest of ${interest:.2f}. New balance is ${self.balance:.2f}")
