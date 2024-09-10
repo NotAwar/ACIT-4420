7e511457087e8345025341c11a98958798873bcc
NotAwar <awar0201@gmail.com>
2024-09-11 00:16:28 +0200

    testing the function

--- Mandatory_Assignment_1/Module1.py
+++ Mandatory_Assignment_1/Module1.py
@@ -9,3 +9,7 @@ def find_prime_factors(n, prime_factors=[]):
     if n > 1:
         prime_factors.append(n)
     return prime_factors
+
+# Call the function and print the output to test
+result = find_prime_factors(56)
+print(result)
