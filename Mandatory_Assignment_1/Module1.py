5371289b015060935305144903be68dfcbb0ccd5
NotAwar <awar0201@gmail.com>
2024-09-11 00:12:13 +0200

    initial task, initial commit

---
+++ Mandatory_Assignment_1/Module1.py
@@ -0,0 +1,11 @@
+def find_prime_factors(n, prime_factors=[]):
+    i = 2
+    while i * i <= n:
+        if n % i == 0:
+            prime_factors.append(i)
+            n //= i
+        else:
+            i += 1
+    if n > 1:
+        prime_factors.append(n)
+    return prime_factors
